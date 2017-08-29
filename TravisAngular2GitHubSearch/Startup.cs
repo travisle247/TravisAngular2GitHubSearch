fd72c3d98e0588205675ab6f97f9dbbbf0e1e7c0
travisle247 <travisle247@hotmail.com>
2017-08-28 20:22:01 -0500

    Check in

--- TravisAngular2GitHubSearch/Startup.cs
+++ TravisAngular2GitHubSearch/Startup.cs
@@ -10,28 +10,31 @@ using Microsoft.Extensions.Logging;
 
 namespace TravisAngular2GitHubSearch
 {
-    public class Startup
+  public class Startup
+  {
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {
-            loggerFactory.AddConsole();
-
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-        }
+      services.AddMvc();
     }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+    {
+      app.Use(async (context, next) => {
+        await next();
+        if (context.Response.StatusCode == 404 &&
+           !System.IO.Path.HasExtension(context.Request.Path.Value) &&
+           !context.Request.Path.Value.StartsWith("/api/"))
+        {
+          context.Request.Path = "/index.html";
+          await next();
+        }
+      });
+      app.UseMvcWithDefaultRoute();
+      app.UseDefaultFiles();
+      app.UseStaticFiles();
+    }
+  }
 }
